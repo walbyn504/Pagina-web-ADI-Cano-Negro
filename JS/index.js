1f818cfdab62870b6d19179b7a619de7470fd27e
walbyn <walbyngonzalez@gmail.com>
2026-02-12 12:15:22 -0600

    Se elimino funciones inecesarias y ademas de agrego una funcion

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,5 @@
 window.addEventListener("scroll", function() {
     const header = document.querySelector(".main-header");
     header.classList.toggle("scrolled", window.scrollY > 50);
-});
-
+})
 window.addEventListener('scroll', revealSection);
