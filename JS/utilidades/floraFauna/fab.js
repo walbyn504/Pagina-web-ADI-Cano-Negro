653763be82131d55a302f22f65b029c2c7f630dd
walbyn <walbyngonzalez@gmail.com>
2026-04-16 13:13:07 -0600

    Se elimino archivos por redudancia, se agrego modal, informacion acerca de Caño Negro (ia), estilos

--- JS/utilidades/floraFauna/fab.js
+++
@@ -1,8 +0,0 @@
-const fabToggle = document.getElementById("fabToggle");
-const fabContainer = document.querySelector(".fab-container");
-
-if (fabToggle && fabContainer) {
-    fabToggle.addEventListener("click", () => {
-        fabContainer.classList.toggle("open");
-    });
-}
